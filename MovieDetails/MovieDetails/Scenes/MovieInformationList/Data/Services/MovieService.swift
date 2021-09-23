59ba6659a01544a619dc6ae16d2c71811a4c8f99
Lucas Nakano <lucas_nakano@hotmail.com>
2021-09-23 11:23:08 -0300

    Introduced Alomofire, Data Request and Poster Request

--- MovieDetails/MovieDetails/Scenes/MovieInformationList/Data/Services/MovieService.swift
+++ MovieDetails/MovieDetails/Scenes/MovieInformationList/Data/Services/MovieService.swift
@@ -1,10 +1,48 @@
 import Foundation
+import Alamofire
 
 final class MovieService {
-    func fetchMoviesDetails() {
+    func fetchMoviesDetails(completion: @escaping (Movie) -> Void) {
         let movieID: Int = 181812
-        guard let url = URL(string: "https://api.themoviedb.org/3/movie/\(movieID)?api_key=76a63be0aae78226a632aacfd082fd6e") else { print("URL inválido")
+        guard let url = URL(string: "https://api.themoviedb.org/3/movie/\(movieID)?api_key=76a63be0aae78226a632aacfd082fd6e")
+        else {
+            print("URL inválido")
             return
         }
+        
+        AF.request(url)
+            .validate()
+            .responseData { response in
+                switch response.result {
+                case .success:
+                    print("Validation Successful")
+                case let .failure(error):
+                    fatalError("Erro fetching: \(error)")
+                }
+            }
+            .responseDecodable(of: Movie.self) { response in
+                guard let movieDetails = response.value else { return }
+                completion(movieDetails)
+            }
+    }
+    
+    func fetchMoviePoster(posterPath: String, width: Int, completion: @escaping (Data) -> Void) {
+        guard let url = URL(string: "https://image.tmdb.org/t/p/w\(width)\(posterPath)")
+        else {
+            print("URL inválido")
+            return
+        }
+        
+        AF.request(url)
+            .validate()
+            .responseData { response in
+                switch response.result {
+                case .success:
+                    print("Validation Successful")
+                case let .failure(error):
+                    fatalError("Erro fetching: \(error)")
+                }
+                completion(response.data!)
+            }
     }
 }
--- MovieDetails/MovieDetails/Scenes/MovieInformationList/View/ViewController.swift
+++ MovieDetails/MovieDetails/Scenes/MovieInformationList/View/ViewController.swift
@@ -1,13 +1,10 @@
 import UIKit
 
 class MovieDetailsViewController: UIViewController {
-
-    let service = MovieService()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        service.fetchMoviesDetails()
     }
 
 
