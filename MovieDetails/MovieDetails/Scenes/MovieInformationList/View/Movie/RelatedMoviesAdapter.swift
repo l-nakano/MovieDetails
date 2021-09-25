37c956cf49ddd80fd87b16e0d755b2a9a9cf42f5
Lucas Nakano <lucas_nakano@hotmail.com>
2021-09-25 14:38:50 -0300

    Saving Liked Movie in UserDefaults

--- MovieDetails/MovieDetails/Scenes/MovieInformationList/View/Movie/RelatedMoviesAdapter.swift
+++ MovieDetails/MovieDetails/Scenes/MovieInformationList/View/Movie/RelatedMoviesAdapter.swift
@@ -12,7 +12,6 @@ final class RelatedMovieAdapter: NSObject, UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let movie = relatedMovies[indexPath.row]
         let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as! RelatedMovieCell
-        print(cell)
         cell.config(using: movie, withGenres: genres)
         return cell
     }
